2199780e2c0ea6c933d4fb1179d1c2c27800217e
joaooliveira247 <joaooliveira247@gmail.com>
2023-07-13 17:41:21 -0300

    :sparkles: update var name from config.settings

--- book_club/core/data_base.py
+++ book_club/core/data_base.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy import create_engine, Engine
 from book_club.core.config import settings
 
-engine: Engine = create_engine(settings.DB_URL, echo=True)
+engine: Engine = create_engine(settings.db_url, echo=True)
 
 DB_Session: Session = sessionmaker(
     autocommit=False, autoflush=False, class_=Session, bind=engine
